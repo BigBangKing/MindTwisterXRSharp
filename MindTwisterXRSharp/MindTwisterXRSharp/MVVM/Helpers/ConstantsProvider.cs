cfee16a50eded31ae05a19400e2abd222dcfef8f
BigBangKing <rifatalmahmud0111@gmail.com>
2023-10-02 10:28:31 +0600

    Add project files.

---
+++ MindTwisterXRSharp/MindTwisterXRSharp/MVVM/Helpers/ConstantsProvider.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindTwisterXRSharp.MVVM
+{
+    internal class ConstantsProvider
+    {
+       public  string[] AUDI = new string[] {
+            @"CarPics\Audi\Audi_A3_2010.jpg",
+            @"CarPics\Audi\Audi_A3_2015_.jpg",
+            @"CarPics\Audi\Audi_A3_2016.jpg",
+            @"CarPics\Audi\Audi_A4_2011.jpg",
+            @"CarPics\Audi\Audi_A4_2011StationWagon.jpg",
+            @"CarPics\Audi\Audi_A4_2013.jpg",
+            @"CarPics\Audi\Audi_A6_2011.jpg",
+            @"CarPics\Audi\Audi_A7_2014.jpg",
+            @"CarPics\Audi\Audi_Q5_2014.jpg",
+            @"CarPics\Audi\Audi_TT_2012.jpg",
+        };
+               
+       public  string[] BENTLEY = new string[] {
+            @"CarPics\Bentley\Bentley_Bentayga_2017.jpg",
+            @"CarPics\Bentley\Bentley_Bentayga_2017_SUV.jpg",
+            @"CarPics\Bentley\Bentley_Bentayga_2017_SUV_qID.jpg",
+            @"CarPics\Bentley\Bentley_Bentayga_2017_SUV_VeX.jpg",
+            @"CarPics\Bentley\Bentley_Bentayga_2020_SUV_iTS.jpg",
+            @"CarPics\Bentley\Bentley_Bentayga_2020_UV_Ssr.jpg",
+            @"CarPics\Bentley\Bentley_Continental GT_2011_Convertible_DiU.jpg",
+            @"CarPics\Bentley\Bentley_Continental GT_2011_Convertible_ePq.jpg",
+            @"CarPics\Bentley\Bentley_Continental GT_2011_Convertible_kkv.jpg",
+            @"CarPics\Bentley\Bentley_Continental GT_2012_Convertible_PSE.jpg",
+        };
+               
+       public  string[] BMW = new string[] {
+            @"CarPics\BMW\BMW_2-Series_2014.jpg",
+            @"CarPics\BMW\BMW_2-Series_2015.jpg",
+            @"CarPics\BMW\BMW_3-Series_2011.jpg",
+            @"CarPics\BMW\BMW_3-Series_2013.jpg",
+            @"CarPics\BMW\BMW_3-Series_2019.jpg",
+            @"CarPics\BMW\BMW_4-Series_2014.jpg",
+            @"CarPics\BMW\BMW_5-Series_2011.jpg",
+            @"CarPics\BMW\BMW_6-Series_2014.jpg",
+            @"CarPics\BMW\BMW_8-Series_2020_.jpg",
+            @"CarPics\BMW\BMW_i3_2014.jpg",
+        };
+               
+       public  string[] FIAT = new string[] {
+            @"CarPics\Fiat\FIAT_124 Spider_2017_Convertible.jpg",
+            @"CarPics\Fiat\FIAT_124 Spider_2019_Convertible.jpg",
+            @"CarPics\Fiat\FIAT_124 Spider_2019_RWDConvertible.jpg",
+            @"CarPics\Fiat\FIAT_500_2011.jpg",
+            @"CarPics\Fiat\FIAT_500_2012.jpg",
+            @"CarPics\Fiat\FIAT_500L_2014.jpg",
+            @"CarPics\Fiat\FIAT_500L_2017.jpg",
+            @"CarPics\Fiat\FIAT_500X_2017.jpg",
+            @"CarPics\Fiat\FIAT_500X_2017_AWD_SUV.jpg",
+            @"CarPics\Fiat\FIAT_500X_2017AWD_5_4_SUV.jpg",
+        };
+               
+       public  string[] FORD = new string[] {
+            @"CarPics\Ford\Ford_Ecosport_2019_4WD_SUV.jpg",
+            @"CarPics\Ford\Ford_Ecosport_2019_SUV.jpg",
+            @"CarPics\Ford\Ford_Edge_2011.jpg",
+            @"CarPics\Ford\Ford_Edge_2014.jpg",
+            @"CarPics\Ford\Ford_Edge_2019.jpg",
+            @"CarPics\Ford\Ford_Escape_201.jpg",
+            @"CarPics\Ford\Ford_Escape_2011.jpg",
+            @"CarPics\Ford\Ford_Escape_20114WD__SUV.jpg",
+            @"CarPics\Ford\Ford_Expedition_2017.jpg",
+            @"CarPics\Ford\Ford_Fiesta_2012_.jpg",
+        };
+               
+       public  string[] HONDA = new string[] {
+            @"CarPics\Honda\Honda_Accord_2011_FWD_2dr.jpg",
+            @"CarPics\Honda\Honda_Accord_2011_FWD_5dr.jpg",
+            @"CarPics\Honda\Honda_Accord_2012.jpg",
+            @"CarPics\Honda\Honda_Accord_2015.jpg",
+            @"CarPics\Honda\Honda_Accord_2020.jpg",
+            @"CarPics\Honda\Honda_Civic_2013.jpg",
+            @"CarPics\Honda\Honda_Civic_2015.jpg",
+            @"CarPics\Honda\Honda_Clarity_2017.jpg",
+            @"CarPics\Honda\Honda_CR-V_2011.jpg",
+            @"CarPics\Honda\Honda_CR-V_2011_.jpg",
+        };
+               
+       public  string[] HYANDAI = new string[] {
+            @"CarPics\Hyundai\Hyundai_Sonata_2011.jpg",
+            @"CarPics\Hyundai\Hyundai_Sonata_2014.jpg",
+            @"CarPics\Hyundai\Hyundai_Sonata_2017.jpg",
+            @"CarPics\Hyundai\Hyundai_Sonata_2019.jpg",
+            @"CarPics\Hyundai\Hyundai_Sonata_2020.jpg",
+            @"CarPics\Hyundai\Hyundai_Tucson_2013.jpg",
+            @"CarPics\Hyundai\Hyundai_Tucson_2019.jpg",
+            @"CarPics\Hyundai\Hyundai_Tucson_2019_.jpg",
+            @"CarPics\Hyundai\Hyundai_Veloster_2012.jpg",
+            @"CarPics\Hyundai\Hyundai_Veloster_2012_.jpg",
+        };
+               
+       public  string[] JAGUAR = new string[] {
+            @"CarPics\Jaguar\Jaguar_E-Pace_2020.jpg",
+            @"CarPics\Jaguar\Jaguar_E-Pace_2020_.jpg",
+            @"CarPics\Jaguar\Jaguar_F-Pace_2019.jpg",
+            @"CarPics\Jaguar\Jaguar_F-Type_2015.jpg",
+            @"CarPics\Jaguar\Jaguar_F-Type_2016.jpg",
+            @"CarPics\Jaguar\Jaguar_I-Pace_2019.jpg",
+            @"CarPics\Jaguar\Jaguar_I-Pace_2020.jpg",
+            @"CarPics\Jaguar\Jaguar_I-Pace_2020_.jpg",
+            @"CarPics\Jaguar\Jaguar_XE_2017.jpg",
+            @"CarPics\Jaguar\Jaguar_XE_2017_.jpg",
+        };
+               
+       public  string[] MERCEDESBENZ = new string[] {
+            @"CarPics\Mercedes_Benz\Mercedes-Benz_C Class_2013.jpg",
+            @"CarPics\Mercedes_Benz\Mercedes-Benz_C Class_2015.jpg",
+            @"CarPics\Mercedes_Benz\Mercedes-Benz_C Class_2015_.jpg",
+            @"CarPics\Mercedes_Benz\Mercedes-Benz_C Class_2016.jpg",
+            @"CarPics\Mercedes_Benz\Mercedes-Benz_C Class_2017.jpg",
+            @"CarPics\Mercedes_Benz\Mercedes-Benz_C Class_2017_.jpg",
+            @"CarPics\Mercedes_Benz\Mercedes-Benz_C Class_2019.jpg",
+            @"CarPics\Mercedes_Benz\Mercedes-Benz_CLA Class_2014.jpg",
+            @"CarPics\Mercedes_Benz\Mercedes-Benz_CLA Class_2014_.jpg",
+            @"CarPics\Mercedes_Benz\Mercedes-Benz_CLA Class_2020.jpg",
+        };
+               
+       public  string[] TOYOTA = new string[] {
+            @"CarPics\Toyato\Toyota_4Runner_2011_.jpg",
+            @"CarPics\Toyato\Toyota_Avalon_2011_.jpg",
+            @"CarPics\Toyato\Toyota_Avalon_2014_.jpg",
+            @"CarPics\Toyato\Toyota_Camry_2013.jpg",
+            @"CarPics\Toyato\Toyota_Camry_2016.jpg",
+            @"CarPics\Toyato\Toyota_Corolla_2011.jpg",
+            @"CarPics\Toyato\Toyota_Highlander_2011.jpg",
+            @"CarPics\Toyato\Toyota_Land Cruiser_2010_.jpg",
+            @"CarPics\Toyato\Toyota_Prius C_2012.jpg",
+            @"CarPics\Toyato\Toyota_Prius_2020.jpg",
+        };
+
+
+        public string ArraysCorrespondingNames(int i)
+        {
+            switch (i)
+            {
+                case 0:
+                    return "AUDI"; 
+
+                case 1:
+                    return "BENTLEY";
+                    
+
+                case 2:
+                    return "BMW";
+                    
+
+                case 3:
+                    return "FIAT";
+                    
+
+                case 4:
+                    return "FORD";
+                    
+
+                case 5:
+                    return "HONDA";
+                    
+                case 6:
+                    return "HYANDAI";
+                    
+
+                case 7:
+                    return "JAGUAR";
+                    
+
+                case 8:
+                    return "MERCEDES BENZ";
+                    
+
+                case 9:
+                    return "TOYOTA";
+                    
+
+                default:
+                    return "AUDI";
+                    
+            }
+        }
+
+        
+        public string[] ArraysCorresponder(int i)
+        {
+            switch (i)
+            {
+                case 0:
+                    return AUDI; 
+
+                case 1:
+                    return BENTLEY;
+                    
+
+                case 2:
+                    return BMW;
+                    
+
+                case 3:
+                    return FIAT;
+                    
+
+                case 4:
+                    return FORD;
+                    
+
+                case 5:
+                    return HONDA;
+                    
+                case 6:
+                    return HYANDAI;
+                    
+
+                case 7:
+                    return JAGUAR;
+                    
+
+                case 8:
+                    return MERCEDESBENZ;
+                    
+
+                case 9:
+                    return TOYOTA;
+                    
+
+                default:
+                    return AUDI;
+                    
+            }
+        }
+
+
+
+    } 
+
+
+}
---
+++ MindTwisterXRSharp/MindTwisterXRSharp/MainPage.xaml.cs
@@ -0,0 +1,210 @@
+﻿using MindTwisterXRSharp.MVVM;
+using System;
+using System.Collections.Generic; 
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using XRSharp.Controls;
+using XRSharp.Controls.Primitives;
+
+namespace MindTwisterXRSharp
+{
+    public partial class MainPage : Page
+    {
+        ConstantsProvider constants;
+        private DispatcherTimer dispatcherTimer;
+
+        int chosenBrand = 0;
+
+        int score = 0;
+        int totalAsked = 0;
+
+        bool canClickAgain = false;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            constants = new ConstantsProvider();
+
+            dispatcherTimer = new DispatcherTimer();
+
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 2); // 2 seconds interval
+
+            PopulateData();
+
+        }
+
+
+        private void ButtonBase3D_Click(object sender, RoutedEventArgs e)
+        {
+            if (!canClickAgain) return;
+
+            string s = ((ButtonBase3D)sender).Tag.ToString();
+
+            totalAsked++;
+            canClickAgain = false;
+
+            switch (s)
+            {
+                case "1":
+
+                    if (chosenBrand == 0)
+                    {
+                        correctAnswer();
+
+                    }
+                    else
+                    {
+                        wrongAnswer();
+                    }
+
+
+                    startTimer();
+                    break;
+                case "2": 
+                    if (chosenBrand == 1)
+                    {
+                        correctAnswer();
+
+                    }
+                    else
+                    {
+                        wrongAnswer();
+
+                    }
+                    startTimer();
+                    break; 
+                case "3":
+                    if (chosenBrand == 2)
+                    {
+                        correctAnswer();
+                    }
+                    else
+                    {
+                        wrongAnswer();
+
+                    }
+                    startTimer();
+                    break;
+                default:
+                    break;
+            }
+
+             
+
+        }
+
+        private void startTimer()
+        {
+            ScoreText.Text = "Score: " + score + " / " + totalAsked;
+            dispatcherTimer.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            // Do something here every second
+            PopulateData();
+
+        }
+
+        private void PopulateData()
+        {
+
+            dispatcherTimer.Stop();
+
+            int MaxLength = 9;
+            int MinLength = 0;
+            List<int> ints = new List<int>();
+            List<int> actualPictures = new List<int>();
+            Random rnd = new Random();
+            string ImagePath  = "";
+            string ImagePath2 = "";
+            string ImagePath3 = "";
+
+
+
+            for (int i = 1; i <= 3; i++)
+            {
+                int randomNumber = rnd.Next(MinLength, MaxLength);
+
+                while (ints.Count > 0 && ints.Contains(randomNumber)) {
+                    randomNumber= rnd.Next(MinLength, MaxLength);
+
+                }
+                // avoid already taken array.
+                ints.Add(randomNumber);
+
+
+                string[] array = constants.ArraysCorresponder(randomNumber);
+
+                //just another random number for actual pictures path:
+                int randomNumberActual = rnd.Next(MinLength, MaxLength);
+                while (actualPictures.Count > 0 && actualPictures.Contains(randomNumberActual))
+                {
+                    randomNumberActual = rnd.Next(MinLength, MaxLength);
+                 
+                
+                }
+                // avoid already taken array.
+                actualPictures.Add(randomNumberActual);
+
+                switch (i)
+                {
+                    case 1: ImagePath = array[randomNumberActual];
+                        break; 
+                    case 2: ImagePath2 = array[randomNumberActual];
+                        break; 
+                    case 3: 
+                        ImagePath3 = array[randomNumberActual];
+                        break;
+
+                }
+
+                //Console.WriteLine(ImagePath +" , "+ImagePath2 +" , "+ImagePath3);
+
+            }
+
+            // just choose any random number to ask the brand of.
+            chosenBrand = rnd.Next(0, ints.Count);
+
+            QText.Text = "Select "+ constants.ArraysCorrespondingNames(ints[chosenBrand]) +" branded car:";
+            AText.Text = "";
+             
+            ButtonImage1.Content = getImageSource(ImagePath);
+             
+            ButtonImage2.Content = getImageSource(ImagePath2);
+             
+            ButtonImage3.Content = getImageSource(ImagePath3);
+
+            canClickAgain = true;
+
+        }
+
+        private Image3D getImageSource(string ImagePath)
+        {
+
+            var image = new Image3D();
+            image.Source = new BitmapImage(new Uri(ImagePath, UriKind.Relative));
+
+            return image;
+        }
+
+        private void correctAnswer()
+        {
+            score++;
+            AText.Foreground = new SolidColorBrush(Colors.DarkCyan);
+            AText.Text = "Correct";
+        }
+
+        private void wrongAnswer()
+        {
+            AText.Foreground = new SolidColorBrush(Colors.Red);
+            AText.Text = "Wrong";
+        }
+
+    }
+}
